d687a7b6ded6ec3999d75045145ff2d1342c8af8
Mgachanja <markgachanja1@gmail.com>
2024-02-22 13:59:23 +0300

    websocket added

--- server/index.js
+++ server/index.js
@@ -3,9 +3,16 @@ const PORT = process.env.PORT ||5000;
 const cors= require('cors')
 const dotenv = require('dotenv').config();
 const app = express();
+const createServer=require('node:https').createServer;
 const connectDB = require('./database configuration/databaseConnection');
 const router=require('./routes/signup.js')
-
+const {Server}=require('socket.io')
+const httpServer = createServer(app);
+const io = new Server(httpServer,{
+    cors:{
+        origin:"localhost:3000"
+    }
+})
 connectDB()
 
 app.use(cors())
@@ -13,6 +20,27 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use('/', router)
 
+
+
+io.on('connection', (socket) => {
+    console.log('New connection')
+    socket.on('disconnect', () => {
+        console.log('User has left')
+    })
+    socket.on('message', (message) => {
+        console.log(message)
+    })
+    socket.emit('message :',message)
+    socket.broadcast.emit('socket', 'A new user has joined')
+}
+
+)
+
+
+
+httpServer.listen(6000, () => {
+    console.log(`Server listening on 6000`)
+})
 
 
 app.listen(PORT , () => {
