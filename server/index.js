5a289c999987cfe4477b5394f8de22e107e42d86
Mgachanja <markgachanja1@gmail.com>
2024-02-27 22:39:04 +0300

    websocket added

--- server/index.js
+++ server/index.js
@@ -1,26 +1,25 @@
 const express =require('express');
-const PORT = process.env.PORT ||5000;
+const PORT = process.env.PORT ||6000;
 const cors= require('cors')
 const dotenv = require('dotenv').config();
 const app = express();
-const createServer=require('node:https').createServer;
-const connectDB = require('./database configuration/databaseConnection');
-const router=require('./routes/signup.js')
-const {Server}=require('socket.io')
-const httpServer = createServer(app);
-const io = new Server(httpServer,{
-    cors:{
-        origin:"localhost:3000"
+const http = require('http')
+const server = http.createServer(app);
+const io=require('socket.io')(server, {
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST']
     }
-})
-connectDB()
+});;
+const connectDB = require('./database configuration/databaseConnection');
+const router=require('./routes/signup.js') 
+
+//connectDB()
 
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use('/', router)
-
-
 
 io.on('connection', (socket) => {
     console.log('New connection')
@@ -28,18 +27,15 @@ io.on('connection', (socket) => {
         console.log('User has left')
     })
     socket.on('message', (message) => {
-        console.log(message)
+       console.log("server",message)
+
     })
-    socket.emit('message :',message)
     socket.broadcast.emit('socket', 'A new user has joined')
 }
-
 )
 
-
-
-httpServer.listen(6000, () => {
-    console.log(`Server listening on 6000`)
+server.listen(4000, () => {
+    console.log(`Server listening on 4000`)
 })
 
 
